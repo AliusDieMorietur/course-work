b4761cd130e43d35b3d56e09b351131e129516e8
AliusDieMorietur <ikamqwert@gmail.com>
2020-03-08 13:53:06 +0200

    changed theme to PWA

---
+++ static/worker.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const files = [
+  '/',
+  '/css/style.css',
+  '/js/client.js',
+  '/favicon.ico',
+  '/favicon.png',
+  '/manifest.json',
+];
+
+
+self.addEventListener('install', event => event.waitUntil(
+  caches.open('v1').then(cache => cache.addAll(files))
+));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    if (response !== undefined) return response;
+    return fetch(event.request).then(response => {
+      const responseClone = response.clone();
+      caches.open('v1').then(cache => {
+        cache.put(event.request, responseClone);
+      });
+      return response;
+    }).catch(error => {
+      throw error;
+    });
+  }));
+});
